6f8a7c68649f2cabe602c4b96535ad10af4adb4b
Molly DeAngelis-Jimenez <55717647+Mollydj@users.noreply.github.com>
2020-06-05 22:17:34 -0400

    feature 1 scenario 3 testing added

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,29 +3,8 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
 
     state = {
-        query: 'Munich',
-        suggestions: [
-                {
-                "city": "Brooklyn",
-                "country": "us",
-                "localized_country_name": "USA",
-                "state": "NY",
-                "name_string": "Brooklyn, New York, USA",
-                "zip": "11201",
-                "lat": 40.7,
-                "lon": -73.99
-                },
-                {
-                "city": "Brooklyn Heights",
-                "country": "us",
-                "localized_country_name": "USA",
-                "state": "OH",
-                "name_string": "Brooklyn Heights, Ohio, USA",
-                "zip": "44131",
-                "lat": 41.38,
-                "lon": -81.66
-                }
-        ]
+        query: '',
+        suggestions: []
     }
 
     handleInputChanged = (event) => {
@@ -34,13 +13,17 @@ class CitySearch extends Component {
         //Takes the value from the input updates the state of 'query' based on that value
     }
 
+    handleItemClicked = (value) => {
+        this.setState({ query: value });
+      }
+
     render() {
         return (
             <div className="CitySearch">
                 <input type="text" className="city" value={this.state.query} onChange={this.handleInputChanged}/>
                     <ul className="suggestions">
                       {this.state.suggestions.map(item =>
-                        <li key={item.name_string}>{item.name_string}</li>
+                        <li key={item.name_string} onClick={() => this.handleItemClicked(item.name_string)}>{item.name_string}</li>
                     )}
                     </ul>
             </div>
--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -3,36 +3,66 @@ import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
 
 describe('<CitySearch /> component', () => {
+
+    let CitySearchWrapper;
+    beforeAll(() => {
+        CitySearchWrapper = shallow(<CitySearch/>);
+    });
+
     test('render text input', () => {
-        const CitySearchWrapper = shallow(<CitySearch/>);
         expect(CitySearchWrapper.find('.city')).toHaveLength(1);
     });
 
     test('render list of suggestions', () => {
-        const CitySearchWrapper = shallow(<CitySearch />);
         expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
     });
 
     test('render text input correctly', () => {
-        const CitySearchWrapper = shallow(<CitySearch/>);
         const query = CitySearchWrapper.state('query');
         expect(CitySearchWrapper.find('.city').prop('value')).toBe(query)
     });
 
     test('change state when text input changes', () => {
-        const CitySearchWrapper = shallow(<CitySearch />);
-        const eventObject = { target: { value: 'Berlin' }};
+        const eventObject = { target: { value: 'Brooklyn' }};
         CitySearchWrapper.find('.city').simulate('change', eventObject);
-        expect(CitySearchWrapper.state('query')).toBe('Berlin');
+        expect(CitySearchWrapper.state('query')).toBe('Brooklyn');
       });
 
       test('render list of suggestions correctly', () => {
-        const CitySearchWrapper = shallow(<CitySearch />);
         const suggestions = CitySearchWrapper.state('suggestions');
         expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length);
         for (let i = 0; i < suggestions.length; i += 1) {
           expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i].name_string);
         }
+      });
+
+      test('click on suggestion should change query state', () => {
+          CitySearchWrapper.setState({
+              suggestions: [
+                {
+                    "city": "Brooklyn",
+                    "country": "us",
+                    "localized_country_name": "USA",
+                    "state": "NY",
+                    "name_string": "Brooklyn, New York, USA",
+                    "zip": "11201",
+                    "lat": 40.7,
+                    "lon": -73.99
+                    },
+                    {
+                    "city": "Brooklyn Heights",
+                    "country": "us",
+                    "localized_country_name": "USA",
+                    "state": "OH",
+                    "name_string": "Brooklyn Heights, Ohio, USA",
+                    "zip": "44131",
+                    "lat": 41.38,
+                    "lon": -81.66
+                    }
+              ]
+          });
+            CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+            expect(CitySearchWrapper.state('query')).toBe('Brooklyn, New York, USA');
       });
 });
 
