40b047b586b7d147a43aa57919e3a0078fc56085
Molly DeAngelis-Jimenez <55717647+Mollydj@users.noreply.github.com>
2020-06-13 00:22:34 -0400

    Updates

---
+++ precache-manifest.48c8ae8eec6d6f1c88a45c4d60e30284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47efba113f4125a3af6d9966a1f8822a",
+    "url": "/Meetup_App/index.html"
+  },
+  {
+    "revision": "7bcff496e13ad58745a7",
+    "url": "/Meetup_App/static/css/main.87539c40.chunk.css"
+  },
+  {
+    "revision": "c4e9b43258805773afdb",
+    "url": "/Meetup_App/static/js/2.4e3a2f98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meetup_App/static/js/2.4e3a2f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bcff496e13ad58745a7",
+    "url": "/Meetup_App/static/js/main.0bf3842c.chunk.js"
+  },
+  {
+    "revision": "93e670fa91b41fa57666",
+    "url": "/Meetup_App/static/js/runtime-main.09384cc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Meetup_App/precache-manifest.a92bdfebb77b52c022b3ef9a6ec4898c.js"
+  "/Meetup_App/precache-manifest.48c8ae8eec6d6f1c88a45c4d60e30284.js"
 );
 
 self.addEventListener('message', (event) => {
