f1c491d248960533cb7619775b872d4ab15ab794
Molly DeAngelis-Jimenez <55717647+Mollydj@users.noreply.github.com>
2020-06-13 15:49:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { getEvents } from './api';
 class App extends Component {
 
 componentDidMount(){
-  getEvents().then(response => this.setState({ events: response, lat, lon }));
+  getEvents(response, lat, lon).then(response => this.setState({ events: response, lat, lon }));
 }
   state = {
     events: [],
