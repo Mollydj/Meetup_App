6b372a395544af2e28a93ec8053e8bf50409f9b0
Molly DeAngelis-Jimenez <55717647+Mollydj@users.noreply.github.com>
2020-06-14 19:49:56 -0400

    All tests now pass

--- src/Event.js
+++ src/Event.js
@@ -12,19 +12,26 @@ class Event extends Component {
       : this.setState({ details: false });
   };
 
-
-  
-
   render() {
     const event = this.props.event;
-    const people = this.props.event.yes_rsvp_count > 1 ? ' people attening' : ' person going';
+    const eventLocation = event.group.localized_location;
+    const people =
+      this.props.event.yes_rsvp_count > 1
+        ? " people attening"
+        : " person going";
 
     return (
       <div className="Event">
-        <p className="timeAndDate">{event.local_time} {event.local_date}</p>
-        <div className="name"><b>{event.name}</b></div>
-        {event.yes_rsvp_count}{people}<br/>
-        <p className="going">{event.group.localized_location}</p>
+        <p className="timeAndDate">
+          {event.local_time} {event.local_date}
+        </p>
+        <div className="name">
+          <b>{event.name}</b>
+        </div>
+        {event.yes_rsvp_count}
+        {people}
+        <br />
+        <p className="going">{eventLocation}</p>
         <div className="EventDetails"></div>
         <button className="details-btn" onClick={this.handleShowDetails}>
           details
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-import Event from '../Event';
-import {mockEvents} from '../mock-events';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "../App";
+import Event from "../Event";
+import { mockEvents } from "../mock-events";
 
-
-describe('<Event /> component', () => {
-
-    let EventWrapper;
-    const events ={
-      created: 1563366230000,
-      duration: 3600000,
-      id: "263222596",
-      name: "1 Million Cups Augusta at theClubhou.se",
-      date_in_series_pattern: false,
-      status: "upcoming",
-      time: 1565179200000,
-      local_date: "2019-08-07",
-      local_time: "08:00",
-      updated: 1563366230000,
-      utc_offset: -14400000,
-      waitlist_count: 0,
-      yes_rsvp_count: 21,
-
-      venue: {
+describe("<Event /> component", () => {
+  let EventWrapper;
+  const events = {
+    created: 1563366230000,
+    duration: 3600000,
+    id: "263222596",
+    name: "1 Million Cups Augusta at theClubhou.se",
+    date_in_series_pattern: false,
+    status: "upcoming",
+    time: 1565179200000,
+    local_date: "2019-08-07",
+    local_time: "08:00",
+    updated: 1563366230000,
+    utc_offset: -14400000,
+    waitlist_count: 0,
+    yes_rsvp_count: 21,
+    venue: {
       id: 25900005,
       name: "Georgia Cyber Center Hull Mcknight Building",
       lat: 33.459999084472656,
@@ -34,35 +31,51 @@ describe('<Event /> component', () => {
       country: "us",
       localized_country_name: "USA",
       zip: "30901",
-      state: "GA"
-      }
-    }
-    beforeAll(() => {
-        EventWrapper = shallow(<Event event={events}/>);
-    });
-
-    // FEATURE 2
-
-    test('details button renders', () => {
-      expect(EventWrapper.find('.detailsButton')).toHaveLength(1);
-    });
-
-    test('show details on click', () => {
-        EventWrapper.find('.detailsButton').at(0).simulate('click');
-        expect(EventWrapper.state('details')).toBe(true);
-      });
-    
-      test('hide details on click', () => {
-        EventWrapper.find('.detailsButton').at(0).simulate('click');
-        expect(EventWrapper.state('details')).toBe(false);
-      });
-  
-    test('set mock eventdata as state', () => {
-      EventWrapper.setState({ events });
-      expect(EventWrapper.state('events').name).toBe('1 Million Cups Augusta at theClubhou.se');
-    });
+      state: "GA",
+    },
+    is_online_event: false,
+    group: {
+      created: 1281014308000,
+      name: 'Williamsburg French Language Group',
+      id: 1686219,
+      join_mode: 'open',
+      lat: 40.709999084472656,
+      lon: -73.94999694824219,
+      urlname: 'Williamsburg-French-Language-Meetup-Group',
+      who: 'Members',
+      localized_location: 'Brooklyn, NY',
+      state: 'NY',
+      country: 'us',
+      region: 'en_US',
+      timezone: 'US/Eastern',
+    },
+  };
   
 
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={events} />);
+  });
 
+  // FEATURE 2
+
+  test("details button renders", () => {
+    expect(EventWrapper.find(".details-btn")).toHaveLength(1);
+  });
+
+  test("show details on click", () => {
+    EventWrapper.find(".details-btn").at(0).simulate("click");
+    expect(EventWrapper.state("details")).toBe(true);
+  });
+
+  test("hide details on click", () => {
+    EventWrapper.find(".details-btn").at(0).simulate("click");
+    expect(EventWrapper.state("details")).toBe(false);
+  });
+
+  test("set mock eventdata as state", () => {
+    EventWrapper.setState({ events });
+    expect(EventWrapper.state("events").name).toBe(
+      "1 Million Cups Augusta at theClubhou.se"
+    );
+  });
 });
-
